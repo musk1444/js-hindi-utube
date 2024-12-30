33415273ad9a0f69e0c56e4c6e04a2ae857813c9
musk1444 <132774661+musk1444@users.noreply.github.com>
2024-12-30 06:13:19 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+// "use strict"; // this means treat all js code as newer version
+
+// alert(3+3) // not valid here as we are using nodejs, not browser
+
+console.log(3+3);
+console.log("Muskaan");
+
+// number ki range --> 2 to the power 53
+// bigint --> bhot large values : trading,stock
+// string --> ""
+// boolen --> han ya na
+// null --> ye ek stand alone value h
+// null ek representation h empty value ka
+// undefined 
+// symbol --> unique 
+
+// object 
+
+// console.log(typeof null); // -> null ek object h
+
+
+
+
