2c1a709992a3ffb1e4600285c062f990cafacf91
musk1444 <132774661+musk1444@users.noreply.github.com>
2024-12-30 07:18:47 +0000

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,21 @@
+// console.log(2>2);
+// console.log(2<=2);
+// console.log(2> 1);
+// console.log(2!= 1);
+
+// console.log(null == 0);
+
+// console.log(null > 0);
+// console.log(null != 0);
+// console.log(null >= 0);
+
+// equality and comparison operator work differently
+// comparison convert the null to a number, as 0
+// so it is treated differently
+// that's why null >= 0 is true wheres null > 0 is false
+
+
+// strict check --> ===
+
+console.log("2" === 2)
+
