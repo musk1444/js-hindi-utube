fc188217698a994b153124662985e32fc7754f42
musk1444 <132774661+musk1444@users.noreply.github.com>
2024-12-30 07:07:24 +0000

    conervsion

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,66 @@
+let score ="33"
+let score2 = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueinnumber = Number(score) // -> converts the string type to number(int)
+let valueinnumber2 = Number(score2) // -> this also has abc in the value so this will also get converted but into NaN
+
+// console.log(typeof valueinnumber);
+// console.log(typeof(valueinnumber));
+// console.log(valueinnumber);
+// console.log(valueinnumber2); // --> results in NaN
+
+// "33" is 33
+// "33abs" is Nan --> not a number
+// true is 1
+
+let isloggedin = " "
+let booleanconvert = Boolean(isloggedin)
+
+// console.log(booleanconvert);
+// 1 -> true, 0-> false
+// "" -> false, "name" -> true
+
+
+let value = 1;
+let stringconvert = String(value);
+// console.log((stringconvert));
+// console.log(typeof stringconvert);
+
+
+/************************************************OPERATIONS********************/
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%3);
+
+
+let str1 ="hello"
+let str2 = " muskan"
+
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);  // -> 12
+// console.log(1 + "2");  // -> 12
+// console.log("1" + 2+ 2 ); // -> 122
+// console.log(1 + 2 + "2"); // -> 32
+
+console.log(+true);
+
+
+
+
+
+
+
+
+
+
+
+
