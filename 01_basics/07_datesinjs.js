2185154684255687c2b32378bb1f4f56c38f5d94
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-01-02 11:33:40 +0000

    datesinjs

---
+++ 01_basics/07_datesinjs.js
@@ -0,0 +1,30 @@
+
+let mydate = new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toISOString());
+
+// let mycreateddate = new Date(2023,0,24)
+// console.log(mycreateddate);
+
+// let mycreateddate = new Date(2023,0,24)
+// console.log(mycreateddate.toDateString());
+
+// let mycreateddate = new Date(2023,0,24,5,3)
+// console.log(mycreateddate.toLocaleString());
+
+// let mycreateddate = new Date("01-04-2023")
+// console.log(mycreateddate.toDateString());
+
+let mytimestamp = Date.now();
+// console.log(mytimestamp);
+
+// let mycreateddate = new Date("01-14-2024")
+// console.log(mycreateddate.getTime());
+
+console.log(Math.round(Date.now()/1000));
+
+
+
+
